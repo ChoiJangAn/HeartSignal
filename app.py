0ccf13370adfa31ac2949d2ee6f6addea8565e10
ye0nse00 <chldustj0427@naver.com>
2024-08-27 18:22:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
 from flask import Flask, render_template, jsonify
 import random
-import threading
-import time
 
 app = Flask(__name__)
-
-# 전역 변수
-heartbeat = 80
-
-def generate_heartbeat():
-    global heartbeat
-    while True:
-        heartbeat = random.randint(80, 150)
-        time.sleep(1)
 
 @app.route('/')
 def index():
@@ -20,9 +9,8 @@ def index():
 
 @app.route('/data')
 def data():
+    heartbeat = random.randint(80, 150)  # 요청 시마다 새 값을 생성
     return jsonify({'heartbeat': heartbeat})
 
 if __name__ == '__main__':
-    # 쓰레드를 사용하여 심박도 생성
-    threading.Thread(target=generate_heartbeat, daemon=True).start()
     app.run(host='0.0.0.0', port=8000)
