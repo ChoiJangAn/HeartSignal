a4071e0d4773143de2e2d5725fb96284d5f18cb8
ye0nse00 <chldustj0427@naver.com>
2024-08-28 20:13:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify
 import threading
 import time
+import os
 
 app = Flask(__name__)
 
@@ -13,18 +14,18 @@ data_index = 0  # 현재 데이터를 가리키는 인덱스
 
 def read_data():
     global data_list
-    try:
-        with open(file_path, 'r') as file:
-            data_list = [line.strip() for line in file if line.strip().isdigit()]
-    except FileNotFoundError:
-        print(f"파일을 찾을 수 없습니다: {file_path}")
-
-# 초기 데이터 읽기
-read_data()
+    if os.path.exists(file_path):
+        try:
+            with open(file_path, 'r') as file:
+                data_list = [line.strip() for line in file if line.strip().isdigit()]
+        except Exception as e:
+            print(f"파일을 읽는 중 오류 발생: {e}")
 
 def generate_heartbeat():
     global data_index, data_list
     while True:
+        # 데이터 갱신
+        read_data()
         if data_list:
             data_index = (data_index + 1) % len(data_list)  # 다음 데이터를 가리키도록 인덱스를 순환시킴
         time.sleep(1)  # 1초마다 인덱스를 갱신
@@ -36,7 +37,6 @@ def index():
 @app.route('/data')
 def data():
     global data_index
-    # 현재 인덱스의 데이터를 반환
     if data_list:
         heartbeat = int(data_list[data_index])  # 인덱스에 해당하는 데이터를 반환
     else:
@@ -44,7 +44,11 @@ def data():
     
     return jsonify({'heartbeat': heartbeat})
 
-if __name__ == '__main__':
+def start_threads():
     # 데이터 갱신을 위한 스레드 시작
     threading.Thread(target=generate_heartbeat, daemon=True).start()
+
+if __name__ == '__main__':
+    # Flask 애플리케이션을 실행하기 전에 스레드를 시작
+    start_threads()
     app.run(host='0.0.0.0', port=8000)
